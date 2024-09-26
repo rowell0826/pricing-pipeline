303b138a6755f02d63a9b6a4b4c41cbeaf487ade
arvee0220 <rowellverdejo@gmail.com>
2024-09-27 02:36:04 +0800

    add smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
+			>
 				{children}
 			</body>
 		</html>
