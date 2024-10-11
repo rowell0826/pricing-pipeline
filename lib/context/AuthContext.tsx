b84388b462b3529d571a7293ac294f7860069582
arvee0220 <rowellverdejo@gmail.com>
2024-10-12 03:32:16 +0800

    Remove console log

--- lib/context/AuthContext.tsx
+++ lib/context/AuthContext.tsx
@@ -27,7 +27,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
 					if (userSnapshot.exists()) {
 						const userData = userSnapshot.data();
-						console.log("User data:", userData);
 
 						if (userData.role) {
 							setRole(userData.role);
