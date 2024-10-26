29ad1320a92da429b4e68f59690552ac96ad64f2
arvee0220 <rowellverdejo@gmail.com>
2024-10-26 22:54:13 +0800

    Change env

--- lib/utils/firebase/firebase.ts
+++ lib/utils/firebase/firebase.ts
@@ -21,12 +21,12 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEYHK,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAINHK,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_IDHK,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKETHK,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_IDHK,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_IDHK,
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
