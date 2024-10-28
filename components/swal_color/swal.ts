d2bc929ee6abc9128c6bf806e4ce81bb60675978
arvee0220 <rowellverdejo@gmail.com>
2024-10-29 00:29:19 +0800

    remove swal.ts

--- components/swal_color/swal.ts
+++
@@ -1,7 +0,0 @@
-export const backgroundColor = getComputedStyle(document.documentElement).getPropertyValue(
-	"--background"
-);
-
-export const textColor = getComputedStyle(document.documentElement).getPropertyValue(
-	"--foreground"
-);
