77265a9f0f979e41d8ba41ff1d08cc8cae8f0ec4
arvee0220 <rowellverdejo@gmail.com>
2024-11-01 01:59:22 +0800

    Add autoArchive function

--- components/droppable/Draggable.tsx
+++ components/droppable/Draggable.tsx
@@ -1,5 +1,6 @@
 import { FileUpload, Task } from "@/lib/types/cardProps";
 import {
+	autoArchiveTickets,
 	clientFileUpload,
 	db,
 	deleteFileFromStorage,
@@ -111,7 +112,13 @@ export const DraggableCard = (props: React.PropsWithChildren<DraggableProps>) =>
 			}
 		};
 
+		const updateTicketsStatus = async () => {
+			await autoArchiveTickets();
+		};
+
 		fetchTaskData();
+		
+		updateTicketsStatus();
 	}, [id]);
 
 	const getFilenameFromUrl = (url: string) => {
